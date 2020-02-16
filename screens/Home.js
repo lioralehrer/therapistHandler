2bce4df499ad2ba66c8488c417e13f9102729926
liora lehrer <liora.lehrer@sergata.com>
2020-02-16 16:10:12 +0200

    pass data from Home to Manager. need to pass also from Manager to Home

--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,91 @@ import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 import { globalStyles } from '../styles/global'
 
 export default class AddMission extends Component {
+    state = {
+        missions: [
+          {
+            text: 'הוסיפי משימות',
+            id: 0,
+            checked: false,
+            tries: '0',
+            succseses: '0',
+          },
+    
+          {
+            text: 'הציגי משימות בעזרת לולאה',
+            id: 1,
+            checked: false,
+            tries: '0',
+            succseses: '0',
+          },
+          {
+            text: 'סמני כל משימה בנפרד',
+            id: 2,
+            checked: false,
+            tries: '0',
+            succseses: '0',
+          },
+          {
+            text: 'העבירי משימות לכרטיסים בעמוד אחר',
+            id: 3,
+            checked: false,
+            tries: '2',
+            succseses: '0',
+          },
+          {
+            text: 'עדיין הרדקודד?',
+            id: 4,
+            checked: false,
+            tries: '1',
+            succseses: '1',
+          }
+        ],
+        myMissions: [],
+      }
+      componentDidMount() {
+        this.setState({
+          myMissions: [...this.state.missions.filter(mission => mission.checked)]
+        })
+      }
+      checkedMission = (id) => {
+        let missions = this.state.missions;
+        missions[id].checked = !missions[id].checked
+        this.setState({
+          missions: missions,
+          myMissions: [...missions.filter(mission => mission.checked)]
+        })
+      }
+      handleTries = (num, key) => {
+        let missions = this.state.missions;
+        missions[key].tries = num.toString();
+        this.setState({
+          missions: missions,
+          myMissions: [...missions.filter(mission => mission.checked)]
+        })
+      }
+      handleSuccesses = (num, key) => {
+        let missions = this.state.missions;
+        missions[key].succseses = num.toString();
+        this.setState({
+          missions: missions,
+          myMissions: [...missions.filter(mission => mission.checked)]
+        })
+      }
+      addMission = (mission) => {
+        let missions = this.state.missions;
+        let newMission = {
+          text: mission,
+          id: missions.length,
+          checked: false,
+          tries: '0',
+          succseses: '0',
+        }
+        this.setState({
+          missions: [...missions, newMission],
+          myMissions: [...missions.filter(mission => mission.checked)]
+        })
+    
+      }
 
     render() {
         return (
@@ -12,8 +97,9 @@ export default class AddMission extends Component {
                     <TouchableHighlight
                         style={globalStyles.circle}
                         underlayColor='#ccc'
-                        onPress={() => this.props.navigation.navigate('Manager')}
-                    >
+                         onPress={() => this.props.navigation.navigate('Manager', { managerName:'זהבה', patient:'אסתי', missions: this.state.missions})}
+                   
+                   >
                         <Text style={styles.text}>  מנהל   </Text>
                     </TouchableHighlight>
                     <TouchableHighlight
--- screens/Manager.js
+++ screens/Manager.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, StyleSheet, View, Text, Modal, ScrollView,Button } from 'react-native';
+import { TouchableHighlight, StyleSheet, View, Text, Modal, ScrollView, Button } from 'react-native';
 import MyHeader from '../components/MyHeader';
 import MissionCheckbox from '../components/MissionCheckbox';
 import AddMission from '../components/AddMission';
@@ -9,11 +9,15 @@ import { globalStyles } from '../styles/global'
 export default class Manager extends Component {
     constructor(props) {
         super(props);
+        // this.managerName = this.props.navigation.state.managerName;
+        // this.missions = props.navigation.state.params.missions
+        
         this.state = {
             modalVisible: false,
 
         };
     }
+   
     addMission = (mission) => {
         this.props.addMission(mission);
     }
@@ -29,69 +33,81 @@ export default class Manager extends Component {
     checkedMission = (id) => {
         this.props.checkedMission(id);
     }
+  
+
     render() {
+        const {managerName} = this.props.route.params;
+        const {patient} = this.props.route.params;
+        const {missions} = this.props.route.params;
+        // const myMissionsList = Object.entries(this.props.myMissions).map(([key, value]) => {
+        //     return <Mission mission={this.props.myMissions[key]}
+        //         handleTries={(num) => this.handleTries(num, key)}
+        //         handleSuccesses={(num) => this.handleSuccesses(num, key)}
+        //     />
+        // })
         return (
             <View style={globalStyles.MainContainer}>
-            <MyHeader title='מנהל טיפול' />
-            <Text style={styles.HeaderInsideText}>
-                שלום {this.props.managerName} {'\n'}
-                המטופל שלך: {this.props.patient}
-            </Text>
+                <MyHeader title='מנהל טיפול' />
+                <Text style={styles.HeaderInsideText}>
+                    שלום {managerName} {'\n'}
+                    המטופל שלך: {patient}
+                </Text>
 
-            <View style={styles.btnContainer}>
-                <TouchableHighlight
-                    style={globalStyles.circle}
-                    underlayColor='#ccc'
-                    onPress={() => this.paciantPicker()}
-                >
-                    <Text>  מטופל אחר </Text>
-                </TouchableHighlight>
-                <TouchableHighlight
-                    style={globalStyles.circle}
-                    underlayColor='#ccc'
-                    onPress={() => alert('Yaay!')}
-                >
-                    <Text> בחר פעילויות </Text>
-                </TouchableHighlight>
+                <View style={styles.btnContainer}>
+                    <TouchableHighlight
+                        style={globalStyles.circle}
+                        underlayColor='#ccc'
+                        onPress={() => this.paciantPicker()}
+                    >
+                        <Text>  מטופל אחר </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={globalStyles.circle}
+                        underlayColor='#ccc'
+                        onPress={() => alert('Yaay!')}
+                    >
+                        <Text> עדכן פעילויות </Text>
+                    </TouchableHighlight>
 
-                <TouchableHighlight
-                    style={globalStyles.circle}
-                    underlayColor='#ccc'
-                    onPress={() => this.setModalVisible(!this.state.modalVisible)}
-                >
-                    <Text> עדכן משימות   </Text>
-                </TouchableHighlight>
+                    <TouchableHighlight
+                        style={globalStyles.circle}
+                        underlayColor='#ccc'
+                        onPress={() => this.setModalVisible(!this.state.modalVisible)}
+                    >
+                        <Text> עדכן משימות   </Text>
+                    </TouchableHighlight>
 
-                <Modal
-                    animationType="fade"
-                    transparent={false}
-                    visible={this.state.modalVisible}
-                    swipeArea={50}
-                >
-                    <View style={styles.modal} onStartShouldSetResponder={() => true}>
-                        <ScrollView>
-                            <MissionCheckbox missions={this.props.missions} checkedMission={(id) => this.checkedMission(id)}
-                            />
-                            <AddMission addMission={(mission) => this.addMission(mission)} />
-                            <TouchableHighlight
-                                style={globalStyles.circle}
-                                underlayColor='#ccc'
-                                onPress={() => this.setModalVisible(!this.state.modalVisible)}
-                            >
-                                <Text > סיים   </Text>
-                            </TouchableHighlight>
+                    <Modal
+                        animationType="fade"
+                        transparent={false}
+                        visible={this.state.modalVisible}
+                        swipeArea={50}
+                    >
+                        <View style={styles.modal} onStartShouldSetResponder={() => true}>
+                            <ScrollView>
+                                <MissionCheckbox missions={missions} checkedMission={(id) => this.checkedMission(id)}
+                                />
+                                {/* <AddMission addMission={(mission) => this.addMission(mission)} />  */}
+                                <TouchableHighlight
+                                    style={globalStyles.circle}
+                                    underlayColor='#ccc'
+                                    onPress={() => this.setModalVisible(!this.state.modalVisible)}
+                                >
+                                    <Text > סיים   </Text>
+                                </TouchableHighlight>
 
-                            <View style={{ flex: 1 }}>
-                                <Text>blabla</Text>
-                            </View>
-                        </ScrollView>
-                    </View>
+                                <View style={{ flex: 1 }}>
+                                    <Text>blabla</Text>
+                                </View>
+                            </ScrollView>
+                        </View>
 
-                </Modal>
-                
-            </View>
+                    </Modal>
+
+                </View>
+                {/* {myMissionsList} */}
                 <Button title="Go back" onPress={() => this.props.navigation.goBack()} />
-        </View>
+            </View>
         )
     }
 }
@@ -103,7 +119,7 @@ const styles = StyleSheet.create({
     },
 
     HeaderInsideText: {
-        color: '#fff',
+        color: '#900',
         fontSize: 18,
         textAlign: 'center',
         padding: 5,
