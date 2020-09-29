170add29bad2db71ec5557bed3dbdb2f37657c0d
HillaShx <hilla.sh@gmail.com>
2020-09-29 17:46:56 +0300

    added sessions endpoints

--- BED/index.js
+++ BED/index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use('/api/users', require('./routes/api/users')); // users
 app.use('/api/patients', require('./routes/api/patients')); // patients
 app.use('/api/goals', require('./routes/api/goals')); // goals
+app.use('/api/sessions', require('./routes/api/sessions')); // sessions
 
 const PORT = process.env.PORT || 5000;
 
